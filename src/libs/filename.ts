5b0cccd3f5ad441a9a09d82480a4694a462ac1e8
0wx <hai@0wx.dev>
2022-10-14 15:42:14 +0700

    fix: wrong file extension and month
    Only needed `.ts` because no react component involve.
    
    Also fixing `month` in `generateFileName`, need to add 1

--- src/libs/filename.ts
+++ src/libs/filename.ts
@@ -1,7 +1,7 @@
 export const generateFileName = () => {
   const date = new Date()
   const year = date.getFullYear().toString()
-  const month = ('0' + date.getMonth()).slice(-2)
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
   const day = ('0' + date.getUTCDate()).slice(-2)
 
   const random = Math.floor(Math.random() * 1e5)
